1fd321d4a3a257265170923fef598b04f56288eb
KesVisser <92203793+KesVisser@users.noreply.github.com>
2022-02-08 13:58:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 st.sidebar.write('Menu')
 add_selectbox = st.sidebar.selectbox('Choose an opening', ('General', 'Indian Defense', 'Modern Defense', "King's Indian Attack", 'Pirc Defense', 'Sicilian Defense Closed'))
-im_sdc_vs = Image.open('/app/streamlit-example/Sicilian Defense Closed - untitled vs titled.png')
 im_id_t = Image.open('/app/streamlit-example/Indian Defense - titled.png') 
 im_id_t_a = Image.open('/app/streamlit-example/Indian Defense - titled - average.png')
 im_id_u = "hi"
@@ -27,22 +26,15 @@ im_md_t = Image.open('/app/streamlit-example/Modern Defense - titled.png')
 im_md_t_a = Image.open('/app/streamlit-example/Modern Defense - titled - average.png')
 im_kia_t = Image.open("/app/streamlit-example/King's Indian Attack - titled.png")
 im_kia_t_a = Image.open("/app/streamlit-example/King's Indian Attack - titled - average.png")
+im_sdc_t = Image.open("/app/streamlit-example/Sicilian Defense Closed - titled.png")
+im_sdc_t_a = Image.open("/app/streamlit-example/Sicilian Defense Closed - titled - average.png")
+im_sdc_u = "hi"
+im_sdc_u_a = "hi" 
+im_sdc_vs = Image.open('/app/streamlit-example/Sicilian Defense Closed - untitled vs titled.png')
 im_pd_t = Image.open('/app/streamlit-example/Pirc Defense - titled.png')
 im_pd_t_a = Image.open('/app/streamlit-example/Pirc Defense - titled - average.png')
-
-if add_selectbox == 'Sicilian Defense Closed':
-  st.title('Openings: Sicilian Defense Closed')
-  st.write('Sicilian Defense Closed')
-  """
-  We now look at the Sicilian Defense Closed. First we look at the titled players.
-  """
-  """
-  Now we look at the untitled players.
-  """
-  """
-  And now we shall compare the two types of players
-  """
-  st.image(im_sdc_vs)
+im_pd_u = "hi"
+im_pd_u_a = "hi"
   
 if add_selectbox == 'Indian Defense':
   st.title('Openings: Indian Defense')
@@ -67,16 +59,33 @@ if add_selectbox == "King's Indian Attack":
   st.title("Openings: King's Indian Attack")
   """
   The King's Indian Attack is only in the top 5 of titled players. Thus we are only going to look at the titled players. 
-  The King's Indian Attack is played 89 times by titled players, and the longest modern defense game is 205 moves.
+  The King's Indian Attack is played 89 times by titled players, and the longest king's indian attack game is 205 moves.
   """
   st.image(im_kia_t)
   st.image(im_kia_t_a)
+  
+if add_selectbox == 'Sicilian Defense Closed':
+  st.title('Openings: Sicilian Defense Closed')
+  """
+  The Sicilian Defense Closed is in the top 5 of untitled players and in the top 5 of titled players. We are first going to have a look at titled players, then at untitled players. 
+  And then we end with a comparison of the 2 types of players.
+  The Sicilian Defense Closed is played 82 times by titled players, and the longest sicilian defense defense game is a 180 moves.
+  """
+  st.image(im_sdc_t)
+  st.image(im_sdc_t_a)
+  """
+  Now we look at the untitled players.
+  """
+  """
+  And now we shall compare the two types of players
+  """
+  st.image(im_sdc_vs)
 
 if add_selectbox == "Pirc Defense":
   st.title("Openings: Pirc Defense")
   """
   The Pirc Defense is only in the top 5 of titled players. Thus we are only going to look at the titled players. 
-  The Pirc Defense is played 78 times by titled players, and the longest modern defense game is 197 moves.
+  The Pirc Defense is played 78 times by titled players, and the longest pirc defense game is 197 moves.
   """
   st.image(im_pd_t)
   st.image(im_pd_t_a)
